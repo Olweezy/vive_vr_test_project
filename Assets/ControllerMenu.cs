90f22f1b63b9e7aec26e3387f639f1875b5b50db
jtomjack93 <jtomjack93@gmail.com>
2016-07-20 18:18:15 -0400

    Josh's changes

---
+++ Assets/ControllerMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using Valve.VR;
+
+public class ControllerMenu : MonoBehaviour {
+
+    SteamVR_TrackedObject obj;
+
+    public GameObject buttonHolder;
+
+    public bool buttonEnabled;
+
+	// Use this for initialization
+	void Awake () {
+        obj = GetComponent<SteamVR_TrackedObject>();
+        buttonHolder.SetActive(false);
+        buttonEnabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    var device = SteamVR_Controller.Input((int)obj.index);
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
+        {
+            if (buttonEnabled == false)
+            {
+                buttonHolder.SetActive(true);
+                buttonEnabled = true;
+            }
+            else if (buttonEnabled == true)
+            {
+                buttonHolder.SetActive(false);
+                buttonEnabled = false;
+            }
+        }
+	}
+}
